3c84465e8d9f27537d7e3454c48322d48d7e41a4
Jahed Ahmed <jahed.public@gmail.com>
2019-06-30 00:45:40 +0100

    chore: remove accidental comments

--- tests/database.rules.ts
+++ tests/database.rules.ts
@@ -381,31 +381,3 @@ const rules = (): Rules => ({
 })
 
 export { rules }
-
-/**
-const createRuleAuthTokenFirebase = (name: string): RuleAuthTokenFirebase => ({
-  identities: {
-    'email': createRuleStringArray(`${name}["email"]`),
-    'phone': createRuleStringArray(`${name}["phone"]`),
-    'google.com': createRuleStringArray(`${name}["google.com"]`),
-    'facebook.com': createRuleStringArray(`${name}["facebook.com"]`),
-    'github.com': createRuleStringArray(`${name}["github.com"]`),
-    'twitter.com': createRuleStringArray(`${name}["twitter.com"]`)
-  },
-  sign_in_provider: createRuleString(`${name}.sign_in_provider`)
-})
-
-const createRuleAuthToken = (name: string): RuleAuthToken => ({
-  email: createRuleString(`${name}.email`),
-  email_verified: () => `${name}.email_verified`,
-  phone_number: createRuleString(`${name}.phone_number`),
-  name: createRuleString(`${name}.name`),
-  sub: createRuleString(`${name}.sub`),
-  firebase: createRuleAuthTokenFirebase(`${name}.firebase`),
-  iss: createRuleString(`${name}.iss`),
-  aud: createRuleString(`${name}.aud`),
-  auth_time: () => `${name}.auth_time`,
-  iat: () => `${name}.iat`,
-  exp: () => `${name}.exp`
-})
- */
